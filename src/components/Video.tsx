70853023d89abe871e7fbc4d6258ab90ae22005d
eduardo3g <eduardosbrasil10@gmail.com>
2022-06-23 00:13:05 -0300

    feat: get lesson data when user clicks on the sidebar's lesson

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -6,16 +6,66 @@ import {
   Atom,
 } from "phosphor-react";
 import { Player, Youtube, DefaultUi } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
 
 import "@vime/core/themes/default.css";
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlut($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      id
+      videoId
+      description
+      teacher {
+        name
+        bio
+        avatarURL
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    };
+  };
+}
+
+interface VideoParams {
+  lessonSlug: string;
+}
+
+export function Video({ lessonSlug: slug }: VideoParams) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug,
+    },
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  console.log("data", data);
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="VYtBXdf53b4" />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -24,27 +74,24 @@ export function Video() {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">
-              Mastering serverless architectures
-            </h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Live streaming to deep divo into serverless architectures with AWS
-              Lambda, API Gateway, and Event Bridge.
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
-                src="https://avatars.githubusercontent.com/u/7993283?v=4"
-                alt="Teacher avatar"
+                src={data.lesson.teacher.avatarURL}
+                alt={`Avatar from the teacher ${data.lesson.teacher.name}`}
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
               />
 
               <div className="leading-relaxed">
                 <strong className="text-2xl font-bold block">
-                  Marcia Villalba
+                  {data.lesson.teacher.name}
                 </strong>
                 <span className="text-gray-200 text-sm block">
-                  Serverless Advocate @ Amazon Web Services
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
