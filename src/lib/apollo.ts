642c3baf779cca1b5b82d6fb7a706cd46e67af1c
eduardo3g <eduardosbrasil10@gmail.com>
2022-06-23 20:21:08 -0300

    refactor: add GraphCMS URI and Token on .env file

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-us-east-1.graphcms.com/v2/cl4qb8xpv3mpy01z4abxa7ga7/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
