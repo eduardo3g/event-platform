e2a3ab3ba5dcf03f4ad308510d4822e0345afaa9
eduardo3g <eduardosbrasil10@gmail.com>
2022-06-22 23:05:39 -0300

    config: set up apollo client to point to real GraphCMS API

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "graphcms-uri.com",
+  uri: "https://api-us-east-1.graphcms.com/v2/cl4qb8xpv3mpy01z4abxa7ga7/master",
   cache: new InMemoryCache(),
 });
